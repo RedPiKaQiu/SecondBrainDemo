63db6588c1eec146a07bf3667c084cfc15fc180d
RedPiKaQiu <zxm1994929@sina.com>
2025-01-02 11:47:31 +0800

    Initial Commit

---
+++ SecondBrainDemo/SecondBrainDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  SecondBrainDemoApp.swift
+//  SecondBrainDemo
+//
+//  Created by pika on 2025/1/2.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SecondBrainDemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SecondBrainDemoTests/SecondBrainDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecondBrainDemoTests.swift
+//  SecondBrainDemoTests
+//
+//  Created by pika on 2025/1/2.
+//
+
+import Testing
+@testable import SecondBrainDemo
+
+struct SecondBrainDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
